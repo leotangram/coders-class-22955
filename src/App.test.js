d4e92bdaac7e553bef78325d5e56d974e12fbac1
Leonardo Omaña <leotangram@gmail.com>
2021-10-05 21:34:56 -0500

    Class 3 - JSX

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/hello coders/i);
+  expect(headerElement).toBeInTheDocument();
+
+  const button = screen.getByText(/oprimir 2/i);
+  expect(button).toBeInTheDocument();
 });
